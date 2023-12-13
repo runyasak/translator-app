07e017c84fce8478b81a85842497ae3b9c1c343e
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-12-13 17:50:09 +0700

    update example

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import BaseField from './components/BaseField.vue'
+import BaseLanguageSelect from './components/BaseLanguageSelect.vue'
+
+const myWord = ref('')
+
+const languageCode = ref('')
+
+const onLanguageChange = (value: string) => (languageCode.value = value)
+</script>
+
 <template>
   <h1>Hello World</h1>
+  <BaseField v-model="myWord" />
+
+  <BaseLanguageSelect :language="languageCode" @language-change="onLanguageChange($event)" />
+
+  <button>Translate !!</button>
 </template>
